0a85cdbaa922b011280ddd366fe9f8d9ad931fe9
Daniel Rivera <Danielf.r97@gmail.com>
2024-06-10 18:11:37 -0500

    chore: Update dependencies and modify clients.go to add receipt and spreadsheet functionality using watermillion router

--- project/main.go
+++ project/main.go
@@ -2,13 +2,11 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"net/http"
 	"os"
+	"os/signal"
 
 	"github.com/ThreeDotsLabs/go-event-driven/common/clients"
-	"github.com/ThreeDotsLabs/go-event-driven/common/clients/receipts"
-	"github.com/ThreeDotsLabs/go-event-driven/common/clients/spreadsheets"
 	commonHTTP "github.com/ThreeDotsLabs/go-event-driven/common/http"
 	"github.com/ThreeDotsLabs/go-event-driven/common/log"
 	"github.com/ThreeDotsLabs/watermill"
@@ -17,6 +15,7 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/redis/go-redis/v9"
 	"github.com/sirupsen/logrus"
+	"golang.org/x/sync/errgroup"
 )
 
 type TicketsConfirmationRequest struct {
@@ -62,7 +61,31 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	logger := watermill.NewStdLogger(false, false)
 
+	router, err := message.NewRouter(message.RouterConfig{}, logger)
+	if err != nil {
+		panic(err)
+	}
+
+	router.AddNoPublisherHandler(
+		"issue-handler",
+		"issue-receipt",
+		issueReceiptSub,
+		func(msg *message.Message) error {
+			err := receiptsClient.IssueReceipt(msg.Context(), string(msg.Payload))
+			return err
+		},
+	)
+	router.AddNoPublisherHandler(
+		"tracker-handler",
+		"append-to-tracker",
+		appendToTrackerSub,
+		func(msg *message.Message) error {
+			err = spreadsheetsClient.AppendRow(msg.Context(), "tickets-to-print", []string{string(msg.Payload)})
+			return err
+		},
+	)
 	e := commonHTTP.NewEcho()
 
 	e.POST("/tickets-confirmation", func(c echo.Context) error {
@@ -84,100 +107,38 @@ func main() {
 				return err
 			}
 		}
-
 		return c.NoContent(http.StatusOK)
 	})
 
-	go func() {
-		messages, err := issueReceiptSub.Subscribe(context.Background(), "issue-receipt")
-		if err != nil {
-			panic(err)
-		}
-
-		for msg := range messages {
-			err := receiptsClient.IssueReceipt(msg.Context(), string(msg.Payload))
-			if err != nil {
-				logrus.WithError(err).Error("failed to issue the receipt")
-				msg.Nack()
-			} else {
-				msg.Ack()
-			}
-		}
-	}()
-
-	go func() {
-		messages, err := appendToTrackerSub.Subscribe(context.Background(), "append-to-tracker")
-		if err != nil {
-			panic(err)
-		}
-
-		for msg := range messages {
-			err = spreadsheetsClient.AppendRow(msg.Context(), "tickets-to-print", []string{string(msg.Payload)})
-			if err != nil {
-				logrus.WithError(err).Error("failed to append to tracker")
-				msg.Nack()
-			} else {
-				msg.Ack()
-			}
-		}
-	}()
-
 	logrus.Info("Server starting...")
+	ctx := context.Background()
+	ctx, cancel := signal.NotifyContext(ctx, os.Interrupt)
+	defer cancel()
 
-	err = e.Start(":8080")
-	if err != nil && err != http.ErrServerClosed {
+	g, ctx := errgroup.WithContext(ctx)
+
+	g.Go(func() error {
+		return router.Run(ctx)
+	})
+
+	g.Go(func() error {
+		err := e.Start(":8080")
+		if err != nil && err != http.ErrServerClosed {
+			return err
+		}
+
+		return nil
+	})
+
+	g.Go(func() error {
+		// Shut down the HTTP server
+		<-ctx.Done()
+		return e.Shutdown(ctx)
+	})
+
+	// Will block until all goroutines finish
+	err = g.Wait()
+	if err != nil {
 		panic(err)
 	}
-}
-
-type ReceiptsClient struct {
-	clients *clients.Clients
-}
-
-func NewReceiptsClient(clients *clients.Clients) ReceiptsClient {
-	return ReceiptsClient{
-		clients: clients,
-	}
-}
-
-func (c ReceiptsClient) IssueReceipt(ctx context.Context, ticketID string) error {
-	body := receipts.PutReceiptsJSONRequestBody{
-		TicketId: ticketID,
-	}
-
-	receiptsResp, err := c.clients.Receipts.PutReceiptsWithResponse(ctx, body)
-	if err != nil {
-		return err
-	}
-	if receiptsResp.StatusCode() != http.StatusOK {
-		return fmt.Errorf("unexpected status code: %v", receiptsResp.StatusCode())
-	}
-
-	return nil
-}
-
-type SpreadsheetsClient struct {
-	clients *clients.Clients
-}
-
-func NewSpreadsheetsClient(clients *clients.Clients) SpreadsheetsClient {
-	return SpreadsheetsClient{
-		clients: clients,
-	}
-}
-
-func (c SpreadsheetsClient) AppendRow(ctx context.Context, spreadsheetName string, row []string) error {
-	request := spreadsheets.PostSheetsSheetRowsJSONRequestBody{
-		Columns: row,
-	}
-
-	sheetsResp, err := c.clients.Spreadsheets.PostSheetsSheetRowsWithResponse(ctx, spreadsheetName, request)
-	if err != nil {
-		return err
-	}
-	if sheetsResp.StatusCode() != http.StatusOK {
-		return fmt.Errorf("unexpected status code: %v", sheetsResp.StatusCode())
-	}
-
-	return nil
 }
